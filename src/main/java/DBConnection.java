4e053a58686f50f65f81e943fbe692268154ab25
Miriasme <johndoe@example.com>
2026-02-18 10:44:05 +0300

    feat(class): add DBConnection to establish connection in the database and application

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public Connection getConnection() {
+        try {
+            String jdbcURl = "jdbc:postgresql://localhost:5432/pudhdown"; //
+            String user = "postgres"; //mini_dish_db_manager
+            String password = "Asmine1402"; //123456
+            return DriverManager.getConnection(jdbcURl, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Teste " + e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
